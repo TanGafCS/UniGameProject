dde4edde9594e3103445af644881da7ffe11e69e
TanGafCS <60125687+TanGafCS@users.noreply.github.com>
2022-07-04 07:37:37 +0100

    Add build files for Windows

---
+++ buildme.bat
@@ -0,0 +1,2 @@
+javac -d classes -cp classes;libs/jsfml.jar src/Animations/*.java src/DungeonGen/*.java src/Entities/*.java src/GameView/*.java src/LaunchGUI/*.java src/Modifiers/*.java src/Sounds/*.java src/Textures/*.java
+PAUSE
---
+++ runme.bat
@@ -0,0 +1,2 @@
+java -cp classes;libs/jsfml.jar src/LaunchGUI/Driver
+PAUSE
